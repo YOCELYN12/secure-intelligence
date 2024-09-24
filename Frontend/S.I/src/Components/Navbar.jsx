c983c0f583b779db3237ed0841d935fd88cf20f5
YOCELYN12 <yocelynrivera9@gmail.com>
2024-09-24 14:02:16 -0600

    Logo

--- Frontend/S.I/src/Components/Navbar.jsx
+++ Frontend/S.I/src/Components/Navbar.jsx
@@ -5,7 +5,11 @@ function Navbar() {
   return (
     <div>
 
-     
+     <nav className='navbar'> 
+
+        <img className='logo' src="https://i.imgur.com/mVC3h38.png" alt="mmmm" />
+
+     </nav>
 
     </div>
   )
