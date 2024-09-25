e06a15acad9b5d8f70fa1187d585a6135923ff19
YOCELYN12 <yocelynrivera9@gmail.com>
2024-09-25 11:45:15 -0600

    Navbar Listo

--- Frontend/S.I/src/Components/Navbar.jsx
+++ Frontend/S.I/src/Components/Navbar.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import '../Styles/Nav.css'
+import { IoPersonAdd } from "react-icons/io5";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 
 function Navbar() {
   return (
-
     <nav className='navbar'>
 
-     <div>
+      <div>
         <img className='logo' src="https://i.imgur.com/mVC3h38.png" alt="mmmm" />
       </div>
-      
+
       <div className='container-logo-search'>
 
         <div className='cont-barra'>
           <input className="search-container" type="text" placeholder="Buscar..." />
-          <button type="submit"></button>
         </div>
 
+        <div className='btn-buscar'>
+          <button className='icon-buscar'><FaMagnifyingGlass /> </button>
+        </div>
       </div>
 
-    </nav>
+      <div className='account'>
+        <button className='btn-account'>
+          <IoPersonAdd className=' icon-account'/><p>Account</p>
+        </button>
+      </div>
 
+      
+
+    </nav>
   )
 }
 
