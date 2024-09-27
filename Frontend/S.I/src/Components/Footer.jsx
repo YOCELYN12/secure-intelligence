c578fbfc441e82e7780c3aeef47bdcef940e2c45
YOCELYN12 <yocelynrivera9@gmail.com>
2024-09-26 23:12:17 -0600

    COMPONENTE FOOTER TERMINADO

--- Frontend/S.I/src/Components/Footer.jsx
+++ Frontend/S.I/src/Components/Footer.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import '../Styles/Foot.css'
+import { HiOutlineMail } from "react-icons/hi";
+import { FiTool } from "react-icons/fi";
+import { BiMessageDetail } from "react-icons/bi";
+import { LuLogIn } from "react-icons/lu";
+
 function Footer() {
   return (
 
     <footer className="footer">
 
       <div className='cont_centrar_footer' >
-
         <div className='cont_footer'>
 
-          <p className='letras_footer'>Servicios</p>
-          <p className='letras_footer'>Soporte</p>
-          <p className='letras_footer'>Company</p>
-          <p className='letras_footer'>Join Us</p>
-          
-        </div>
+          <a className='letras_footer' href="">Servicios</a>
+          <a className='letras_footer' href="">Soporte</a>
+          <a className='letras_footer' href="">Company</a>
+          <a className='letras_footer' href="">Join Us</a>
 
+        </div>
+      </div>
+
+
+      <div className='cont-icon'>
+        <div className='cont2-iconos'>
+
+          <HiOutlineMail className='iconos-footer' />
+          <FiTool className='iconos-footer'/>
+          <BiMessageDetail className='iconos-footer' />
+          <LuLogIn className='iconos-footer' />
+
+        </div>
       </div>
 
       <p className='letras_footer'>© 2024 Tu Empresa. Todos los derechos reservados.</p>
