e952cd56da1760522ccfc37caeaf72834216ef2b
YOCELYN12 <yocelynrivera9@gmail.com>
2024-09-30 10:46:33 -0600

    ESTILOS DEL LOGIN TERMINADOS

--- Frontend/S.I/src/Pages/Cuentas.jsx
+++ Frontend/S.I/src/Pages/Cuentas.jsx
@@ -8,21 +8,25 @@ function Cuentas() {
 
       <div className='cont_login1'>
 
-        <div className="login-container">
+        <div className="login-container2">
 
           <form className="login-form">
 
-
-            <div className='div-Login'>
+            <div className='div-letras-login'>
               <h2 className='letras_login'>Login to your account</h2>
             </div>
 
-            <div className='cont-input'>
+            <div className='cont-input-login'>
+              
+              <p className='letras-input'>Nombre</p>
+              <input style={{ backgroundColor: "transparent",border:"3px solid blue" }} className='input_login' type="text"  required />
 
-              <input style={{backgroundColor: "transparent"}}     className='input_login' type="text" placeholder="Nombre de usuario" required />
-              <input      className='input_login' type="password" placeholder="Contraseña" required   />
+              <p className='letras-input'>Password</p>
+              <input style={{ backgroundColor: "transparent",border:"3px solid blue" }} className='input_login' type="password" required />
 
             </div>
+
+            <p className='login-acceso'>Already have an account? <link rel="stylesheet" href="Login" /></p>
 
             <button className='button_login' type="submit">Entrar</button>
 
@@ -32,18 +36,30 @@ function Cuentas() {
 
       </div>
 
-
       <div className='cont1_registro'>
 
         <div className="registro-container">
 
           <form className="registro-form">
 
-            <h2 className='letras_registro'>Iniciar Sesión</h2>
-            <input className='input_registro' type="text" placeholder="Nombre de usuario" required />
-            <input className='input_registro' type="password" placeholder="Contraseña" required />
-            <button className='button_registro' type="submit">Entrar</button>
+            <div className='div-registro'>
+              <h2 className='letras_registro'>Create an account</h2>
+            </div>
 
+            <div className='cont-input-registro'>
+              <p className='letras-input' >Nombre</p>
+              <input style={{ backgroundColor: "transparent",border:"3px solid blue" }} className='input_registro' type="text"  required />
+
+              <p className='letras-input'>Password</p>
+              <input style={{ backgroundColor: "transparent",border:"3px solid blue" }} className='input_registro' type="password"  required />
+
+              <p className='letras-input'>Email</p>
+              <input style={{ backgroundColor: "transparent",border:"3px solid blue" }} className='input_registro' type="text"  required/>
+            </div>
+
+            <p className='letras_footer'>Already have an account? <a href="Login"> </a></p>
+
+            <button className='button_registro' type="submit">Entrar</button>
           </form>
 
         </div>
---
+++ Frontend/S.I/src/Pages/PaginaAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PaginaAdmin() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PaginaAdmin
