2f454daad24e0463a10568aaa6b9e4197b00222d
YOCELYN12 <yocelynrivera9@gmail.com>
2024-10-05 12:29:53 -0600

    CARD SERVICIOS LISTAS

--- Frontend/S.I/src/Components/Cards.jsx
+++ Frontend/S.I/src/Components/Cards.jsx
@@ -12,8 +12,8 @@ function Cards() {
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <img src="https://i.imgur.com/JYFA4H6.jpg" className="card-img-top" alt=""/>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <img src="https://i.imgur.com/JYFA4H6.jpg" className="card-img-top" style={{borderRadius:"20px"}} alt=""/>
+                    <a href="#" className="btn btn-primary" style={{display:'flex',justifyContent:'center', marginTop:'10px', borderRadius:'20px'}}>Go somewhere</a>
                 </div>
 
 
