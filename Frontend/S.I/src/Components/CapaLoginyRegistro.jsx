a1ce41746386e73940567b88eb680f757692e12d
YOCELYN12 <yocelynrivera9@gmail.com>
2024-10-21 10:15:30 -0600

    ESTILOS CAPA LOGIN TERMINADOS

--- Frontend/S.I/src/Components/CapaLoginyRegistro.jsx
+++ Frontend/S.I/src/Components/CapaLoginyRegistro.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import '../Styles/CapaLoginyRegistro.css'
-import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 
 
@@ -11,21 +9,7 @@ function CapaLoginyRegistro() {
 
     return (
         <div>
-            <div className='capa1'>
-                <div className='capa2'>
-                    <h1 className='h1'> Welcome <br /> Start your journey <br /> now with our <br /> management <br /> system</h1>
-                    <div className='btn-capas'>
-                        <div className='btn1'>
-                            <button style={{ border: "none", background: "transparent" }} ><MdKeyboardDoubleArrowLeft /></button>
-                        </div>
-
-                        <div className='btn2' >
-                            <button style={{ border: "none", background: "transparent" }} ><MdKeyboardDoubleArrowRight /></button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            
         </div>
     )
 }
--- Frontend/S.I/src/Pages/Cuentas.jsx
+++ Frontend/S.I/src/Pages/Cuentas.jsx
@@ -3,6 +3,8 @@ import Login from '../Components/Login'
 import Registro from '../Components/Registro'
 import '../Styles/Cuentas.css'
 import CapaLoginyRegistro from '../Components/CapaLoginyRegistro'
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 function Cuentas() {
 
@@ -10,14 +12,30 @@ function Cuentas() {
   return (
 
     <div className='bodys'>
-      <CapaLoginyRegistro/> 
-
+ 
+  
 
 
 
 
       <Login />
-       
+      <div className='capa1'>
+        <div className='capa2'>
+          <h1 className='h1'> Welcome <br /> Start your journey <br /> now with our <br /> management <br /> system</h1>
+          <div className='btn-capas'>
+            <div className='btn1'>
+              <button style={{ border: "none", background: "transparent" }} ><MdKeyboardDoubleArrowLeft /></button>
+            </div>
+
+            <div className='btn2' >
+              <button style={{ border: "none", background: "transparent" }} ><MdKeyboardDoubleArrowRight /></button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
       <Registro />
 
     </div>
