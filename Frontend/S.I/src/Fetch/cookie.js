3e11e45101eaf113d0f1158158dae790afefd41c
YOCELYN12 <yocelynrivera9@gmail.com>
2024-11-01 10:22:45 -0600

    COOKIES DOCUMENTADAS

--- Frontend/S.I/src/Fetch/cookie.js
+++ Frontend/S.I/src/Fetch/cookie.js
@@ -1,25 +1,26 @@
-const crearCookie=(nombre,valor,dias)=>{
-    let expira = ""
+const crearCookie = (nombre, valor, dias) => {
+    let expira = "";
     if (dias) {
         let date = new Date();
-        date.setTime(date.getTime() + (dias * 24 * 60 * 60 * 1000));
-        expira = "; expires=" + date.toUTCString();
+        date.setTime(date.getTime() + (dias * 24 * 60 * 60 * 1000)); // Calcula la fecha de expiración
+        expira = "; expires=" + date.toUTCString(); // Formatea la fecha para la cookie
     }
-    document.cookie = nombre + "=" + (valor || "") + expira + "; path=/";
+    document.cookie = nombre + "=" + (valor || "") + expira + "; path=/"; // Crea la cookie
 }
-export {crearCookie}
+export { crearCookie } // Exporta la función para su uso en otros módulos
 
-const traerCookie=(nombre)=> {
-    let nameEQ = nombre + "=";
-    let cookies = document.cookie.split(';');
+
+const traerCookie = (nombre) => {
+    let nameEQ = nombre + "="; // Define el formato de búsqueda
+    let cookies = document.cookie.split(';'); // Divide las cookies en un array
     for (let i = 0; i < cookies.length; i++) {
         let c = cookies[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length); // Elimina espacios en blanco
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length); // Retorna el valor de la cookie
     }
-    return null;
+    return null; // Retorna null si la cookie no se encuentra
 }
-export {traerCookie}
+export { traerCookie } // Exporta la función para su uso en otros módulos
 
 
 
