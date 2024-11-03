9fc601e72f387eb705d94a7514958e6f2dd41bf9
YOCELYN12 <yocelynrivera9@gmail.com>
2024-11-02 23:43:20 -0600

    ESTILOS DE LA PAGINA QUIENES SOMOS TERMINADOS

--- Frontend/S.I/src/Pages/QuienesSomos.jsx
+++ Frontend/S.I/src/Pages/QuienesSomos.jsx
@@ -6,17 +6,13 @@ import Footer from '../Components/Footer'
 function QuienesSomos() {
     return (
 
-        <div>
-
-            <Navbar />
+        <div className='fondo-quienes-somos'>
 
             <div className='img-fondos'>
+                <br />
+                <Navbar />
                 <div className='cont-palabras'>
-                    <div className='letras-quienesSomos'>
-                        <h1 className='h1'>Quienes somos?</h1>
-                        <h6 className='h6'>Solo desarrollamos software?</h6>
-                    </div>
-
+                    <h1 className='h1'>Quienes somos?</h1>
                 </div>
             </div>
 
@@ -25,7 +21,7 @@ function QuienesSomos() {
                 <p className='palabras-quienesSomos'>En Secure Intelligence, somos un equipo apasionado de expertos en tecnología comprometidos con la innovación y la excelencia. Nos dedicamos a la creación de software y desarrollo de páginas web que no solo cumplen con las expectativas de nuestros clientes, sino que las superan.</p>
             </div>
 
-            <img className='img-clarlas' src="https://i.imgur.com/5SXZoYY.jpg" alt="" />
+            <img className='imge-clarlas' src="https://i.imgur.com/5SXZoYY.jpg" alt="" />
             <p className='letras-clarlas' >Además de nuestras capacidades de desarrollo, ofrecemos una gama de servicios educativos, incluyendo cursos de hacking ético y ciberseguridad, donde capacitamos a individuos y empresas en las mejores prácticas para protegerse en el entorno digital. Creemos que el conocimiento es la mejor defensa, y estamos comprometidos a formar a la próxima generación de profesionales en tecnología.</p>
 
 
@@ -44,18 +40,14 @@ function QuienesSomos() {
             </div>
 
             <p className='Ubicacion '> Encuentranos en: <br /> Pozos Santa Ana, escuela republica de Francia, 200 metros este <br />
-            Nuestro horario: <br />Lunes - Viernes de 9am - 5pm </p>
-
-
-
+                Nuestro horario: <br />Lunes - Viernes de 9am - 5pm 
+            </p>
             
-
-
-            <div className='cont-footer'>
-
+            <footer>
                 <Footer />
+            </footer>
 
-            </div>
+
 
         </div>
     )
